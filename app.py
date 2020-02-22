8ccc227a85ed6dce04fea968b2f4fcd2e37c841a
rits1272 <ritikjain1272@gmail.com>
2020-02-23 01:12:12 +0530

    feedback and content api

--- app.py
+++ app.py
@@ -15,29 +15,50 @@ CORS(app)
 ######  MODELS ######
 
 class Teacher(db.Model):
-    __tablename__ = 'teacher'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.relationship('Names', backref="teacher", cascade="all, delete-orphan" , lazy='dynamic')
+    name = db.Column(db.String(90), nullable=False)
+    feedbacks = db.relationship('Feedback', backref='teacher',
+                                cascade="all, delete-orphan", lazy='dynamic')
 
     def __repr__(self):
-        return "<Teacher %r>"% self.name
+        return "<Teacher %r>"% self.id
 
 
 class Feedback(db.Model):
-    __tablename__ = 'feedback'
 
     id = db.Column(db.Integer, primary_key=True)
-    ques_1 = db.Column(db.String(150), nullable=False)
-    ques_2 = db.Column(db.String(150), nullable=False)
-    ques_3 = db.Column(db.String(150), nullable=False)
-    ques_4 = db.Column(db.String(150), nullable=False)
-    opinion = db.Column(db.String(300), nullable=True)
-    teacher = db.Column(db.String, db.ForeignKey('teacher.name'), nullable=False)
+    ques_1 = db.Column(db.Integer, nullable=False)
+    ques_2 = db.Column(db.Integer, nullable=False)
+    ques_3 = db.Column(db.Integer, nullable=False)
+    ques_4 = db.Column(db.Integer, nullable=False)
+    opinion = db.Column(db.Text, nullable=True)
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'))
 
     def __repr__(self):
-        return "<Feedback %r>"% self.opinion
+        return "<Feedback %r>"% self.id
 
+class Content(db.Model):
+    _tablename_ = 'content'
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(150), nullable=False)
+    topic = db.Column(db.String(500), nullable=False)
+    video_content = db.Column(db.Boolean)
+    content = db.Column(db.Text)
+    votes = db.Column(db.Integer, default=0)
+
+##### API #####
+
+@app.route('/get_content', methods=['GET'])
+def get_content():
+    sub = request.args.get('sub', type = str)
+    subject_resp = Content.query.filter_by(subject=sub).all()
+    resp_list = []
+    for i in subject_resp:
+        resp_list.append(i._dict_)
+        
+    return make_response(str(resp_list))
 
 ##### API #####
 
@@ -56,7 +77,7 @@ def save_feedback():
     
     if positive_score > 0.6:
         feedback_obj = Feedback(ques_1=ques_1, ques_2=ques_2, ques_3=ques_3,
-                                ques_4=ques_4, opinion=opinion, teacher=teacher_obj)
+                                ques_4=ques_4, opinion=opinion, teacher=teacher_obj.id)
         db.session.add(feedback_obj)
         db.session.commit()
 
